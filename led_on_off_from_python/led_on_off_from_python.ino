ec2e6915c6be51c2edc1224deb0237f0cbce4fef
indraagustian <56149654+indraagustian@users.noreply.github.com>
2020-07-04 22:46:55 +0700

    Add files via upload

---
+++ led_on_off_from_python/led_on_off_from_python.ino
@@ -0,0 +1,19 @@
+char serialData;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  if(Serial.available()>0){
+    serialData=Serial.read();
+    Serial.print(serialData);
+
+    if(serialData=='1')digitalWrite(LED_BUILTIN, HIGH);
+    else if(serialData=='0')digitalWrite(LED_BUILTIN, LOW);
+  }
+}
