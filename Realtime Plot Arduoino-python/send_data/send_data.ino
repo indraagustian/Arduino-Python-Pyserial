de3a1993cd944396df0e4d24879526af14f2802f
indraagustian <56149654+indraagustian@users.noreply.github.com>
2020-08-21 05:14:18 +0700

    Add files via upload

---
+++ Realtime Plot Arduoino-python/send_data/send_data.ino
@@ -0,0 +1,56 @@
+unsigned long timer = 0;
+long loopTime = 5000;   // microseconds
+int led = 9;           // the PWM pin the LED is attached to
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5; 
+
+void setup() {
+  Serial.begin(9600);
+  timer = micros();
+}
+ 
+void loop() {
+  timeSync(loopTime);
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  double val = random(0,255);
+  //Serial.println(val);
+  delay(10);
+  sendToPC(&val); // send data to python
+}
+ 
+void timeSync(unsigned long deltaT)
+{
+  unsigned long currTime = micros();
+  long timeToDelay = deltaT - (currTime - timer);
+  if (timeToDelay > 5000)
+  {
+    delay(timeToDelay / 1000);
+    delayMicroseconds(timeToDelay % 1000);
+  }
+  else if (timeToDelay > 0)
+  {
+    delayMicroseconds(timeToDelay);
+  }
+  else
+  {
+      // timeToDelay is negative so we start immediately
+  }
+  timer = currTime + timeToDelay;
+}
+ 
+void sendToPC(int* data)
+{
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData, 2);
+}
+ 
+void sendToPC(double* data)
+{
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData, 4);
+}
